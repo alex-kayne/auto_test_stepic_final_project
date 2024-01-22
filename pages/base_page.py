c3797ca5c0f0f685e1d0d73a7ba650b30a7a8039
kang <kanaa@trcont.ru>
2024-01-22 16:34:36 +0300

    added method which check whether an element is presented or not

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
-
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage:
-    def __init__(self, browser: webdriver, url: str):
+    def __init__(self, browser: webdriver, url: str, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how: str, what: str):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
